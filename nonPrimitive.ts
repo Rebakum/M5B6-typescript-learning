444555923c93f85219412f86f43586e7505cc559
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-11-13 20:46:54 +0600

    non primitive data type  litereral types and access modifie

--- nonPrimitive.ts
+++ nonPrimitive.ts
@@ -16,3 +16,46 @@ let MezbaNameAndRoll: [string, number] = ["Mezba", 45];
 // 3 element bisisto tuple
 let coordinates: [number, number, number] = [20, 30, 50];
 let destination: [string, string, number] = ["Dhaka", "chattogram", 3];
+
+// Object Requered full fill korte hobe
+// const user: {
+//   firstName: string;
+//   midleName: string;
+//   lastName: string;
+// } = {
+//   firstName: "Mezbaul",
+//   midleName: "Abedin",
+//   lastName: "forhan",
+// };
+// Object Requered full fill korte hobe
+const user: {
+  organization: "Programming Hero";
+  // value=> type litereral types
+  firstName: string;
+  midleName?: string; // optional
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "Programming Hero",
+  firstName: "Jhankar",
+  lastName: "Mahbub",
+  isMarried: true,
+};
+user.organization = "Programming Hero";
+console.log(user);
+
+const user1: {
+  readonly organization: string;
+  // access modifier
+  firstName: string;
+  midleName?: string; // optional
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "Programming Hero",
+  firstName: "Jhankar",
+  lastName: "Mahbub",
+  isMarried: true,
+};
+user.organization = "Programming Hero";
+console.log(user1);
